12e7519562b567178429e8a668e8e1ba98b095f1
Julien Louis <julienlouis.becode@gmail.com>
2021-05-02 20:00:36 +0200

    update json

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -30110,7 +30110,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37613" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39011" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
